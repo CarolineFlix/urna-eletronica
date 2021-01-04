01a75c207dc18ba82b3e331c47b26aac323d4ffd
Caroline Felix <iamcarolinefelix@gmail.com>
2021-01-04 19:49:39 -0300

    adicionando pasta urna

---
+++ urna/etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '45455',
+        name: "Bruno Covas",
+        partido: "PSDB",
+        fotos: [{ url:"bruno.png", legenda: "Vereador", small: false }],
+      },
+      {
+        numero: '50500',
+        name: "Guilherme Boulos",
+        partido: "PSOL",
+        fotos: [{ url: "boulos.png", legenda: "Vereador" }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '99',
+        name: "Ciclano",
+        partido: "ABC",
+        Vice: "Cic",
+        fotos: [
+          { url: "outro.png", legenda: "Prefeito" },
+          { url: "", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: '84',
+        name: "Zulano",
+        partido: "QWERTY",
+        vice: "Zul",
+        fotos: [
+          { url: "lala", legenda: "Prefeito" },
+          { url: "lala", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
